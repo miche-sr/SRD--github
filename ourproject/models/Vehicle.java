6f82e97172dd7740d79654a4b507176d843311af
matteo paiano <m.paiano1@studenti.unipi.it>
2020-12-11 18:34:06 +0100

    venerdì 11: TreeSet e TrajectoryEnvelope

---
+++ ourproject/models/Vehicle.java
@@ -0,0 +1,277 @@
+package se.oru.coordination.coordination_oru.ourproject.models;
+
+import se.oru.coordination.coordination_oru.ourproject.algorithms.*;
+import se.oru.coordination.coordination_oru.motionplanning.ompl.ReedsSheppCarPlanner;
+
+import org.metacsp.multi.spatioTemporal.paths.Pose;
+import org.metacsp.multi.spatioTemporal.paths.PoseSteering;
+import org.metacsp.multi.spatioTemporal.paths.TrajectoryEnvelope.SpatialEnvelope;
+import org.metacsp.multi.spatioTemporal.paths.TrajectoryEnvelope;
+import org.metacsp.multi.spatioTemporal.paths.TrajectoryEnvelopeSolver;
+import org.metacsp.multi.spatioTemporal.paths.Trajectory;
+
+import com.vividsolutions.jts.geom.Coordinate;
+
+import java.util.*;
+
+public class Vehicle {
+
+	public static enum Category {CAR, AMBULANCE};
+	private static final Coordinate[] fpCar = {new Coordinate(0, 0), new Coordinate(0, 1), 
+											   new Coordinate(1, 1), new Coordinate(1, 0)};
+	private static final Coordinate[] fpAmb = {new Coordinate(0, 0), new Coordinate(0, 3), 
+			   								   new Coordinate(2, 3), new Coordinate(2, 0)};
+	
+	// VARIABILI FISICHE E PROPRIE DEL VEICOLO
+	private int ID = -1;
+	private int priority = -1;
+	private double radius = -1.0;
+	private double secForSafety = -1.0;	// length of trajectory to share
+	private int Tc;						// [ ms ]
+	private double velocity = 0.0;		// [  m/s  ]
+	private double velMax = 0.0;		// [  m/s  ]
+	private double accMax = 0.0;		// [ m/s^2 ]
+	private Coordinate[] footprint;
+
+	// VARIABILI DI PERCORSO E TRAIETTORIA
+	private int pathIndex = 0;			// index of the last pose passed
+	private Pose pose;
+	private Pose start;
+	private Pose[] goal;
+	private PoseSteering[] path;
+	private double[] myTimes;
+	//private SpatialEnvelope se;			// path with envelope
+	private ArrayList<PoseSteering> truncatedPath = new ArrayList<PoseSteering>();
+	private TrajectoryEnvelopeSolver solver = new TrajectoryEnvelopeSolver(0, 100000000);
+	private TrajectoryEnvelope te = null;
+	
+	// VARIABILI PER LE SEZIONI CRITICHE
+	private int criticalPoint = -1;		// -1 if no critical point
+	//private boolean csTooClose = false;
+    private TreeSet<CriticalSection> cs = new TreeSet<CriticalSection>();
+	private CriticalSectionsFounder intersect = new CriticalSectionsFounder();
+
+	// DA USARE PER I VICINI
+	private ArrayList<Vehicle> vehicleList = new ArrayList<Vehicle>();
+	private ArrayList<Vehicle> vehicleNear = new ArrayList<Vehicle>();
+	
+	private KinematicMotion km = new KinematicMotion();
+
+	
+	// COSTRUTTORE
+	// @param distanceTraveled The distance traveled so far along the current current path.
+	public Vehicle(int ID, Category category, Pose start, Pose[] goal) {
+		this.ID = ID;
+		this.pose = start;
+		this.start = start;
+		this.goal = goal;
+		
+		switch (category) {
+			case CAR:
+				this.velMax = 2.0;
+				this.accMax = 1.0;
+				this.priority = 1;
+				this.Tc = 2000;
+				this.footprint = fpCar;
+				break;
+			
+			case AMBULANCE:
+				this.velMax = 4.0;
+				this.accMax = 2.0;
+				this.priority = 2;		// lowest priority wins
+				this.Tc = 1000;
+				this.footprint = fpAmb;
+				break;
+		
+			default:
+            	System.out.println("Unknown vehicle");
+		}
+		double stopTimeMax = this.velMax/this.accMax;
+		this.radius = (2*this.Tc/1000 + stopTimeMax)*this.velMax;
+		this.path = createWholePath();
+		//setmyTimes();
+		//setSpatialEnvelope();
+	}
+	
+	
+	/**********************************************
+	** SET & GET PER VARIABILI FISICHE E PROPRIE **
+	***********************************************/
+	public int getID() {
+		return ID;
+	}
+	public int getTc() {
+		return Tc;
+	}
+	public double getRadius() {
+		return radius;
+	}
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+	public double getSecForSafety() {
+		return secForSafety;
+	}
+	public void setSecForSafety(double secForSafety) {
+		this.secForSafety = secForSafety;
+	}
+	public int getPriority() {
+		return priority;
+	}
+	public double getVelocity() {
+		return velocity;
+	}
+	public void setVelocity(double velocity) {
+		this.velocity = velocity;
+	}
+	public double getVelMax() {
+		return velMax;
+	}
+	public double getAccMAx() {
+		return accMax;
+	}
+	public Coordinate[] getFootprint() {
+		return footprint;
+	}
+	
+	
+	/*****************************************
+	** SET & GET PER PERCORSO E TRAIETTORIA **
+    ******************************************/
+	public Pose getPose() {
+		return pose;
+	}
+	public void setPose(double x, double y, double theta) {
+		this.pose = new Pose(x,y,theta);
+	}
+	public void setPose(Pose pose) {
+		this.pose = pose;
+	}
+	public PoseSteering[] createWholePath(){
+		ReedsSheppCarPlanner rsp = new ReedsSheppCarPlanner();
+		rsp.setRadius(0.2);
+		rsp.setTurningRadius(4.0);
+		rsp.setDistanceBetweenPathPoints(0.5);
+		rsp.setFootprint(this.footprint);
+		rsp.setStart(this.start);
+		rsp.setGoals(this.goal);
+		if (!rsp.plan()) throw new Error ("No path between " + this.start + " and " + this.goal);
+		return rsp.getPath();
+	}
+	public PoseSteering[] getWholePath() {
+		return path;
+	}
+	public TrajectoryEnvelope getTrajectoryEnvelope() {
+		return te;
+	}
+	public void setSpatialEnvelope() {
+		this.truncatedPath.clear();
+		int i = 0;
+		while((pathIndex+i < path.length) && (myTimes[pathIndex+i]-myTimes[pathIndex] <= secForSafety)) {
+			this.truncatedPath.add(path[pathIndex+i]);
+			i++;
+		}
+		PoseSteering[] truncatedPathArray = truncatedPath.toArray(new PoseSteering[truncatedPath.size()]);
+		this.te = solver.createEnvelopeNoParking(this.ID, truncatedPathArray, "Driving", this.footprint);
+	}	
+	public int getPathIndex() {
+		return pathIndex;
+	}
+	public void setPathIndex(int pathIndex) {
+		this.pathIndex = pathIndex;
+	}
+	public double[] getMyTimes() {
+		return myTimes;
+	}
+	public void setMyTimes() {
+		Trajectory traj = new Trajectory(path);
+		this.myTimes = traj.getDTs();
+		for(int i = 1; i < path.length; i++) {
+			this.myTimes[i] += this.myTimes[i-1];
+		}
+	}
+	
+	
+	/**************************************
+	** SET & GET PER LE SEZIONI CRITICHE **
+	***************************************/
+	public TreeSet<CriticalSection> getCs() {
+		return cs;
+	}
+	public void appendCs(Vehicle v2) {
+		CriticalSection[] cs = intersect.findCriticalSections(this, v2);
+		for (CriticalSection c : cs)
+	        this.cs.add(c);
+	}
+	public void clearCs() {
+		this.cs.clear();
+	}
+	public int getCriticalPoint() {
+		return criticalPoint;
+	}
+	public void setCriticalPoint(int criticalPoint) {
+		this.criticalPoint = criticalPoint;
+	}
+	/*public void setCriticalPoint(Boolean prec) {
+		if (this.cs.size() != 0){
+			int t1s = this.cs.first().getTe1Start();
+			if (prec)
+				this.criticalPoint = -1;
+			else
+				this.criticalPoint = t1s;
+		}
+		else
+			this.criticalPoint = -1;
+	}
+	*/
+	public void setCriticalPoint(CriticalSection cs) {
+		this.criticalPoint = cs.getTe1Start();
+	}
+	/*
+	public boolean getCsTooClose() {
+		return csTooClose;
+	}
+	public void setCsTooClose(boolean csTooClose) {
+		this.csTooClose = csTooClose;
+	}	*/
+	
+	
+	/***************************
+	** SET & GET PER I VICINI **
+	****************************/
+	public void setVehicleList(ArrayList<Vehicle> vehicleList) {
+		this.vehicleList = vehicleList;
+	}
+	public ArrayList<Vehicle> getNears (){
+		this.vehicleNear.clear();
+		double vhX, vhY,dist;
+		double x = this.getPose().getX();
+		double y = this.getPose().getY();
+		for (Vehicle vh : this.vehicleList){
+			vhX = vh.getPose().getX();
+			vhY = vh.getPose().getY();
+			dist = Math.sqrt(Math.pow((x - vhX),2.0) + Math.pow((y - vhY),2.0));
+			if (dist <= this.radius && dist > 0){
+				this.vehicleNear.add(vh);
+			}
+		}
+		return vehicleNear;
+	}
+	
+	/*****************
+	** MOTO VEICOLO **
+	******************/
+	public void moveVehicle(Boolean prec) {
+		km.moveVehicle(prec, this);
+		/*
+		if (this.pathIndex < this.criticalPoint){
+			this.pathIndex = pathIndex + 1;
+		}
+		setNewCriticalPoint(prec);
+		if (this.pathIndex < this.getSpatialEnvelope().getPath().length-1){
+			setPose(this.getSpatialEnvelope().getPath()[pathIndex].getPose());
+		}
+		*/
+	}
+	
+}
---
+++ ourproject/models/VehicleThread.java
@@ -0,0 +1,78 @@
+package se.oru.coordination.coordination_oru.ourproject.models;
+
+public class VehicleThread implements Runnable {
+	
+	private Vehicle v;
+	
+	public VehicleThread(Vehicle v){
+		this.v = v;
+	}
+	
+	// MAIN ALGORITHM
+	public void run() {
+		String List;
+		//v.setPathIndex(0);
+		//System.out.println(v.getSecForSafety());
+		try{
+			while(v.getPathIndex() < v.getWholePath().length){		// this will be while true
+				v.setMyTimes();
+				v.setSpatialEnvelope();
+				Thread.sleep(v.getTc());
+
+				//System.out.println(Arrays.toString(v.getMyTimes()));
+				//System.out.println(v.getSpatialEnvelope().getPath().length);
+				
+				List = "";
+				
+				v.clearCs();
+				for (Vehicle vh : this.v.getNears()){
+					v.appendCs(vh);
+					List = (List + vh.getID() + " " );
+				}
+				
+				Boolean prec = true;
+				v.setCriticalPoint(-1);
+				for (CriticalSection cs : this.v.getCs()){
+					prec = cs.ComputePrecedences();
+					if (prec == false)
+						v.setCriticalPoint(cs);
+						break;
+				}
+				
+				v.moveVehicle(prec);
+				
+				printLog(List, prec);
+			}
+			System.out.println("\n R" + this.v.getID() + " : GOAL RAGGIUNTO" );
+		}
+		catch (InterruptedException e) {
+			System.out.println("Thread interrotto");
+		}	
+	}
+
+	
+	public void printLog(String List, Boolean prec) {
+		String CsString = "";
+		if (v.getCs().size() != 0){
+			int i = 0;
+			for (CriticalSection cs : v.getCs()) {
+				CsString = CsString + (i+1 +"° Sezione Critica" +
+				"\t Mia: " + cs.getTe1Start()+"-"+cs.getTe1End() +
+				"\t R" + cs.getVehicle2().getID() + ":" +
+				"\t" + cs.getTe2Start()+"-"+cs.getTe2End()) + "\n";
+				i += 1;
+			}
+		}
+		else
+			CsString = ("0 Sezioni Critiche");
+		
+		System.out.println("\n R" + this.v.getID() + " : \n" + 
+			"Vicini: "  + List + "\n" + 
+			"Precedenza: " + prec + "\n" + 
+			"Path Index: " 	+ v.getPathIndex() + "\n" +
+			"Critical Point: " + v.getCriticalPoint() + "\n" +
+			CsString
+			);
+	}
+}
+
