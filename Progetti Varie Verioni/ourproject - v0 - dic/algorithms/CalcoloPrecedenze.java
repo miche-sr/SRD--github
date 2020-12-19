ec315f92921e8b4b20a13be71ca06617a61653e0
Michele <m.cecchi13@studenti.unipi.it>
2020-12-19 12:24:04 +0100

    Progetti varie versioni

---
+++ Progetti Varie Verioni/ourproject - v0 - dic/algorithms/CalcoloPrecedenze.java
@@ -0,0 +1,51 @@
+package se.oru.coordination.coordination_oru.ourproject.algorithms;
+
+import se.oru.coordination.coordination_oru.ourproject.models.CriticalSection;
+import se.oru.coordination.coordination_oru.ourproject.models.Vehicle;
+
+public class CalcoloPrecedenze {
+	
+	public Boolean  CalcoloPrecedenze(CriticalSection cs) {
+        Boolean prec;
+        Vehicle v1 = cs.getVehicle1();
+        Vehicle v2 = cs.getVehicle2();
+        int pathIndx1 = v1.getPathIndex();
+        int pathIndx2 = v2.getPathIndex();
+        int te1start = cs.getTe1Start();
+        int te2start = cs.getTe2Start();
+        int cp1 = v1.getCriticalPoint();
+        int cp2 = v2.getCriticalPoint();
+
+        if (cp1 > te1start && cp2 > te2start){
+            //ERRORE !! si stanno scontrando
+        }
+        if (cp1 > te1start){
+            prec = true;
+        }
+        else if (pathIndx2 > cs.getTe2End()){
+            prec = true;
+        }
+        else if (cp2 > te2start ){
+            prec = false;
+        }
+        else{
+            if (v1.getPriority() > v2.getPriority()){
+                prec = true;
+            }
+            else if (v2.getPriority() > v1.getPriority()){
+                prec = false;
+            }
+            else{
+                if ((te1start - pathIndx1) < (te2start - pathIndx2)){
+                    prec = true;
+                }
+                else{
+                    prec = false;
+                }
+            }
+
+        }
+        
+        return prec;
+    }
+}
---
+++ Progetti Varie Verioni/ourproject - v1 - 14dicM/Test.java
@@ -0,0 +1,56 @@
+package se.oru.coordination.coordination_oru.ourproject;
+
+import se.oru.coordination.coordination_oru.ourproject.models.*;
+import java.util.*;
+
+import org.metacsp.multi.spatioTemporal.paths.Pose;
+
+public class Test{
+	
+	private static ArrayList<Vehicle> vehicleList = new ArrayList<Vehicle>();	    
+	
+	public static Thread initThread(int id, Vehicle.Category ctg, Pose start, Pose[] goal){
+		Vehicle vehicle = new Vehicle(id, ctg, start, goal);
+		Thread thread = new Thread(new VehicleThread(vehicle));
+		vehicleList.add(vehicle);
+		return thread;
+	}
+	
+
+	public static void main(String args[]) {
+	
+	Vehicle.Category a = Vehicle.Category.AMBULANCE; 
+	Vehicle.Category c = Vehicle.Category.CAR; 
+	
+	Pose start1 = new Pose(0, 0, 0); Pose[] goal1 = {new Pose(30, 0, 0)};
+	Pose start2 = new Pose(5, 8, -Math.PI/2); Pose[] goal2 = {new Pose(5, -4, -Math.PI/2)};
+	Pose start3 = new Pose(8, -8, Math.PI/2); Pose[] goal3 = {new Pose(8, 8, Math.PI/2)};
+	//Pose start4 = new Pose(0, -5, -Math.PI/2); Pose[] goal4 = {new Pose(15, 10, -Math.PI/2)};
+	Pose start4 = new Pose(10, -10, -Math.PI/2); Pose[] goal4 = {new Pose(10, 10, -Math.PI/2)};
+
+	Thread thread1 = initThread(1, a, start1, goal1);
+	//Thread thread3 = initThread(3, c, start3, goal3);
+	//Thread thread4 = initThread(4, c, start4, goal4);
+	Thread thread2 = initThread(2, c, start2, goal2);
+	
+	double rMax = -1; double tMax = -1;
+	for (Vehicle vh : vehicleList){
+		double r = vh.getRadius();
+		if (r > rMax) {
+			rMax = r;
+			tMax = r/vh.getVelMax();
+		}
+	}
+	for (Vehicle vh : vehicleList){
+		vh.setRadius(rMax);
+		vh.setSecForSafety(tMax);
+		vh.setVehicleList(vehicleList);
+	}
+	System.out.println("\n" + "Radius "  + rMax );
+	
+	thread1.start();
+	thread2.start();
+	//thread3.start();
+	//thread4.start();
+	}
+}
